52bd54651586e12950de141d20898daab8e5557c
BadbCatha <inia.em@gmail.com>
2018-01-04 22:25:46 +0100

    first commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ApplicationRecord
+	validates :title, presence: true, length: { minimum: 1 }
+	validates :premiere_date, presence: true, length: { minimum: 4 }
+	validates :length, presence: true, length: { minimum: 2 }
+	validates :genres, presence: true, length: { minimum: 4 }
+	validates :director, presence: true, length: { minimum: 5 }
+	validates :screenwriter, presence: true, length: { minimum: 5 }
+	validates :country, presence: true, length: { minimum: 4 }
+	validates :text, presence: true, length: { minimum: 20 }
+	
+end
 end
